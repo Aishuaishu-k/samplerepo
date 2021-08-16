da3ab74f85ea480b6342aab88f3f7d7949a0187c
aishw <aishw@LAPTOP-5BDPDTMJ>
2021-08-16 13:31:46 +0530

    first commit

---
+++ src/main/java/demoproject/sampleprogram.java
@@ -0,0 +1,11 @@
+package demoproject;
+
+public class sampleprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+
+	}
+
+}
