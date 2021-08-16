0a1996abc84914211809ad07f4acd1e527fd21ea
aishw <aishw@LAPTOP-5BDPDTMJ>
2021-08-16 15:50:12 +0530

    Commit all

---
+++ src/main/java/demoproject/Array.java
@@ -0,0 +1,13 @@
+package demoproject;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {34,67,89};
+		for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]);
+
+	}
+
+}
